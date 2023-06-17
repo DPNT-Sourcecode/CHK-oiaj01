825eb5d436e778f5a3fc7e305230272e0ca92ab1
sbx_user1051 <sbx_user1051@169.254.10.117>
2023-06-17 15:55:19 +0000

    Sat Jun 17 15:55:19 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,32 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+from typing import Union
+
+
+NumberLike = Union[int, float]
+
+
+def compute(x: NumberLike, y: NumberLike) -> NumberLike:
+    """
+    Adds two numbers together.
+
+    Parameters
+    ----------
+    x : Union[int, float]
+        The first number to add.
+    y : Union[int, float]
+        The second number to add.
+
+    Returns
+    -------
+    Union[int, float]
+
+    Raises
+    ------
+    AssertionError
+        If either of the inputs are not ints or floats.
+
+    """
+    assert all([isinstance(x, (int, float)), isinstance(y, (int, float))]), "Inputs must be ints or floats."
+
+    return x + y
+
