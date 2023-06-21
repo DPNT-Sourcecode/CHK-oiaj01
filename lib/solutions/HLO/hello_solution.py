bd5abaf99a447edac9cf9161251f4b56815f0627
sbx_user1051 <sbx_user1051@169.254.251.49>
2023-06-21 11:36:21 +0000

    Wed Jun 21 11:36:21 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,7 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    raise NotImplementedError()
+def hello(friend_name: str) -> str:
+    assert isinstance(friend_name, str), "friend_name must be a string"
+
+    return f"Hello, {friend_name}!"
--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -2,5 +2,10 @@ import pytest
 
 from solutions.HLO import hello_solution
 
+
+def test_refuses_non_string() -> None:
+    with pytest.raises(AssertionError):
+        hello_solution.hello(1)
+
 def test_says_hello() -> None:
     assert hello_solution.hello("John") == "Hello, John!"
