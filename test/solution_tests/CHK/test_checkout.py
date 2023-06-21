ffc9674115103811e5a399f327fda808a75d8fcb
sbx_user1051 <sbx_user1051@169.254.251.49>
2023-06-21 11:52:04 +0000

    Wed Jun 21 11:52:04 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -2,3 +2,24 @@ import pytest
 
 from solutions.CHK import checkout_solution
 
+def test_does_individual_checkout() -> None:
+    assert checkout_solution.checkout("A") == 50
+    assert checkout_solution.checkout("B") == 30
+    assert checkout_solution.checkout("C") == 20
+    assert checkout_solution.checkout("D") == 15
+
+def test_does_checkout_with_multiple_items_no_discounts() -> None:
+    assert checkout_solution.checkout("ABCD") == 115
+
+def test_does_checkout_with_multiple_items_with_discounts() -> None:
+    assert checkout_solution.checkout("AAA") == 130
+    assert checkout_solution.checkout("BB") == 45
+    assert checkout_solution.checkout("AAAAAA") == 260
+    assert checkout_solution.checkout("AAAAA") == 230
+    assert checkout_solution.checkout("AAAA") == 180
+    assert checkout_solution.checkout("BBBB") == 90
+    assert checkout_solution.checkout("BBB") == 75
+    assert checkout_solution.checkout("BBBAA") == 175
+
+def test_does_checkout_with_invalid_items() -> None:
+    assert checkout_solution.checkout("E") == -1
